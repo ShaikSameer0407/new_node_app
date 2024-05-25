5b95be42a15493ed1b8320f6567ef25ac51ae5f5
Sameer <22PA1A12G4@vishnu.edu.in>
2024-05-25 14:44:38 +0530

    Added express to application

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Sameer')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
